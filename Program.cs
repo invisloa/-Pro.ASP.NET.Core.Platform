fa18ea7db7e847ceb2bf3477b2581a3a9afdf576
invisloa <91217120+invisloa@users.noreply.github.com>
2023-03-28 00:14:54 +0200

    shortCircuited requestPipeline

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Use(async (context, next) =>
-{
-	if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
-	{
-		context.Response.ContentType = "text/plain";
-		await context.Response.WriteAsync("Custom Middleware \n");
-	}
+app.Use(async (context, next) => {
 	await next();
+	await context.Response
+	.WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
 });
-
+app.Use(async (context, next) => {
+	if (context.Request.Path == "/short")
+	{
+		await context.Response
+		.WriteAsync($"Request Short Circuited");
+	}
+	else
+	{
+		await next();
+	}
+});
 app.UseMiddleware<Platform.QueryStringMiddleWare>();
 
 app.MapGet("/", () => "Hello World!");
