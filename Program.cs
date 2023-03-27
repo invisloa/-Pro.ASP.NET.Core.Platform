d20ffb5ed0721e64e7a511a7f25fce00b7f92965
invisloa <91217120+invisloa@users.noreply.github.com>
2023-03-28 00:05:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+	if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+	{
+		context.Response.ContentType = "text/plain";
+		await context.Response.WriteAsync("Custom Middleware \n");
+	}
+	await next();
+});
+
+app.UseMiddleware<Platform.QueryStringMiddleWare>();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
