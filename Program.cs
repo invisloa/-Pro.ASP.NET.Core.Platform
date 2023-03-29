6aa134bfbef898b0811b598135f876ba76f84a48
invisloa <91217120+invisloa@users.noreply.github.com>
2023-03-29 17:32:12 +0200

    Dependency Injection singleton

--- Program.cs
+++ Program.cs
@@ -1,18 +1,15 @@
 using Platform;
+using Platform.Services;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<RouteOptions>(opts => {
-	opts.ConstraintMap.Add("countryName",
-	typeof(CountryRouteConstraint));
-});
 var app = builder.Build();
-app.Map("{number:int}", async context => {
-	await context.Response.WriteAsync("Routed to the int endpoint");
-}).Add(b => ((RouteEndpointBuilder)b).Order = 1);
-app.Map("{number:double}", async context => {
-	await context.Response
-	.WriteAsync("Routed to the double endpoint");
-}).Add(b => ((RouteEndpointBuilder)b).Order = 2);
-app.MapFallback(async context => {
-	await context.Response.WriteAsync("Routed to fallback endpoint");
+app.UseMiddleware<WeatherMiddleware>();
+app.MapGet("middleware/function", async (context) => {
+	await TypeBroker.Formatter.Format(context,
+	"Middleware Function: It is snowing in Chicago");
+});
+app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
+app.MapGet("endpoint/function", async context => {
+	await TypeBroker.Formatter.Format(context,
+	"Endpoint Function: It is sunny in LA");
 });
 app.Run();
