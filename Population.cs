15a62b85e41d76dec4e81eb804a6c57986c2e4a4
invisloa <91217120+invisloa@users.noreply.github.com>
2023-03-31 21:50:12 +0200

    loggermessage attribute

--- Population.cs
+++ Population.cs
@@ -1,30 +1,38 @@
-﻿public class Population
-{
-	public static async Task Endpoint(HttpContext context)
-	{
-		string? city = context.Request.RouteValues["city"] as string ?? "london";
+﻿using System.Net.NetworkInformation;
 
-		int? pop = null;
-		switch (city.ToLower())
+namespace Platform
+{
+	public class Population
+	{
+		public static async Task Endpoint(HttpContext context,
+		ILogger<Population> logger)
 		{
-			case "london":
-				pop = 8_136_000;
-				break;
-			case "paris":
-				pop = 2_141_000;
-				break;
-			case "monaco":
-				pop = 39_000;
-				break;
-		}
-		if (pop.HasValue)
-		{
-			await context.Response
-			.WriteAsync($"City: {city}, Population: {pop}");
-		}
-		else
-		{
-			context.Response.StatusCode = StatusCodes.Status404NotFound;
+			logger.LogDebug($"Started processing for {context.Request.Path}");
+			string city
+			= context.Request.RouteValues["city"] as string ?? "london";
+			int? pop = null;
+			switch (city.ToLower())
+			{
+				case "london":
+					pop = 8_136_000;
+					break;
+				case "paris":
+					pop = 2_141_000;
+					break;
+				case "monaco":
+					pop = 39_000;
+					break;
+			}
+			if (pop.HasValue)
+			{
+				await context.Response
+				.WriteAsync($"City: {city}, Population: {pop}");
+			}
+			else
+			{
+				context.Response.StatusCode = StatusCodes.Status404NotFound;
+			}
+			logger.LogDebug($"Finished processing for {context.Request.Path}");
 		}
 	}
 }
