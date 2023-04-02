95774e2cbf43e2c7bc51017129bb9e6cbce47967
invisloa <91217120+invisloa@users.noreply.github.com>
2023-04-02 23:16:48 +0200

    database context

---
+++ Models/Calculation.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Platform.Models
+{
+	public class Calculation
+	{
+		public long Id { get; set; }
+		public int Count { get; set; }
+		public long Result { get; set; }
+	}
+}
--- SumEndpoint.cs
+++ SumEndpoint.cs
@@ -1,16 +1,30 @@
-﻿namespace Platform
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Platform.Services;
+using Platform.Models;
+namespace Platform
 {
 	public class SumEndpoint
 	{
-		public async Task Endpoint(HttpContext context)
+		public async Task Endpoint(HttpContext context,
+		CalculationContext dataContext)
 		{
 			int count;
 			int.TryParse((string?)context.Request.RouteValues["count"],
 			out count);
-			long total = 0;
-			for (int i = 1; i <= count; i++)
+			long total = dataContext.Calculations?
+			.FirstOrDefault(c => c.Count == count)?.Result ?? 0;
+			if (total == 0)
 			{
-				total += i;
+				for (int i = 1; i <= count; i++)
+				{
+					total += i;
+				}
+				dataContext.Calculations?.Add(new()
+				{
+					Count = count,
+					Result = total
+				});
+				await dataContext.SaveChangesAsync();
 			}
 			string totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
 			await context.Response.WriteAsync(
